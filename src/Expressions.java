bb8a7a22f7cb6b4c3d66c7a311797655340c0191
Nagenderbabu <nagenderbabu34@gmail.com>
2017-02-13 19:17:37 +0530

    Evaluating the given expressions

---
+++ src/Expressions.java
@@ -0,0 +1,20 @@
+
+public class Expressions {
+
+	public static void main(String[] args){
+		int x=5;
+		int y=10;
+		
+		int a=x+y*2;
+		System.out.println(a);
+
+		int b=x-y+2;
+		System.out.println(b);
+		
+		int c=(x+y)*2;
+		System.out.println(c);
+	
+	    int d=y%x;
+	    System.out.print(d);
+	}
+}
