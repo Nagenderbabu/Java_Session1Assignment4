eba8a300f836bc780455e089bcb2ee5388abd7e9
Nagenderbabu <nagenderbabu34@gmail.com>
2017-03-22 12:14:08 +0530

    Solving Equations

--- src/Expressions.java
+++ src/Expressions.java
@@ -2,10 +2,13 @@
 public class Expressions {
 
 	public static void main(String[] args){
+		// initializing two integers x and y variables
 		int x=5;
 		int y=10;
-		
+		// In this first expression the operator precedence follows
+		//so firstly * operator 
 		int a=x+y*2;
+		
 		System.out.println(a);
 
 		int b=x-y+2;
